09bdda50bfc3f3d931074f3f1339072dc6d8954c
Devashish <humantch56@gmail.com>
2023-07-02 22:47:50 +0530

    Added responsive navbar and footer

--- src/components/body/BodyMain.jsx
+++ src/components/body/BodyMain.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import NavMain from "../navbar/NavMain";
 import "./BodyMain.css";
 import { Container, Row, Col, Image, FormControl } from "react-bootstrap";
@@ -10,10 +10,31 @@ import Footer from "../footer/Footer";
 import Products from "../products/Products";
 
 const BodyMain = () => {
+
+  const [introSize, setIntroSize] = useState(0);
+  const introRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth <= 1012)
+        {
+          setIntroSize(introRef.current.offsetHeight);
+        }
+    };
+    
+    window.addEventListener('resize', handleResize);
+    
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <Container id="intro" fluid>
-        <NavMain />
+      <Container id="intro" fluid ref={introRef}>
+        <NavMain sidebarHeight={introSize} />
         <Container fluid id="intro-section">
           <Row>
             <Col className="col-lg-6" id="intro-left-content">
@@ -23,7 +44,7 @@ const BodyMain = () => {
                 </div>
 
                 <div className="intro-p">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                  <p>Based out of Gurgaon, we specialize in tailoring custom products, catering to your requirements. Our goal is to provide an optimal solution which meets your needs.</p>
                 </div>
 
                 <div className="intro-buttons">
@@ -50,76 +71,80 @@ const BodyMain = () => {
       </Container>
 
       <Container id="about-section" fluid>
-        <Row>
-          <Col className="col-lg-6" id="about-left-content">
+        
+          <Container className="col-lg-12" id="about-content-outer" fluid>
+            <Col className="col-lg-6" id="about-left-content">
 
-            <Fade left cascade>
-              <div className="about-h2">
-                  <h2>About Us</h2>
-              </div>
+              <Fade left cascade>
+                <div className="about-h2">
+                    <h2>About Us</h2>
+                </div>
 
-              <div className="about-h1">
-                  <h1>Our designs solve problems</h1>
-              </div>
+                <div className="about-h1">
+                    <h1>Our designs solve problems</h1>
+                </div>
 
-              <div className="about-p">
-                  <p>
-                    Mr. R. K. Pal, Technical Director, holds a B.Tech degree in Mechanical Engineering from IIT Kharagpur and has an extensive experience of over 40 years in manufacturing with leading organizations such as Maruti Suzuki, GM, Hyundai, and others. 
-                    He specializes in Lean/TPM/TPS and other world-class manufacturing systems.
-                    Mr. H. K. Tyagi holds a Diploma in Mechanical Engineering and a B.Tech in Mechanical Engineering with a specialization in Computer-Integrated Manufacturing (CIM), 
-                    in addition to an MBA in Operations. He has been working with auto and electronic industries since 1996 and has held lead and consultation roles in Industrial Engineering with other leading organizations in manufacturing.
-                  </p>
-              </div>
-            </Fade>
-
-          </Col>
-          <Col className="col-lg-6" id="about-right-content">
-            <div className="about-img">
-              <Fade right>
-                <img src="about_1.png" alt=""  />
+                <div className="about-p">
+                    <p>
+                      Mr. R. K. Pal, Technical Director, holds a B.Tech degree in Mechanical Engineering from IIT Kharagpur and has an extensive experience of over 40 years in manufacturing with leading organizations such as Maruti Suzuki, GM, Hyundai, and others. 
+                      He specializes in Lean/TPM/TPS and other world-class manufacturing systems.
+                      Mr. H. K. Tyagi holds a Diploma in Mechanical Engineering and a B.Tech in Mechanical Engineering with a specialization in Computer-Integrated Manufacturing (CIM), 
+                      in addition to an MBA in Operations. He has been working with auto and electronic industries since 1996 and has held lead and consultation roles in Industrial Engineering with other leading organizations in manufacturing.
+                    </p>
+                </div>
               </Fade>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="col-lg-4 about-cards">
-            <Fade bottom cascade>
-              <div className="about-card">
-                <div>
-                  <h1>Mission</h1>
-                </div>
-                <div>
-                  <p>Our aim is to become the top industrial equipment supplier in India, offering a wide range of products that help reduce process costs.</p>
-                </div>
+
+              </Col>
+              <Col className="col-lg-6" id="about-right-content">
+              <div className="about-img">
+                <Fade right>
+                  <img src="about_1.png" alt=""  />
+                </Fade>
               </div>
-            </Fade>
-          </Col>
-          <Col className="col-lg-4 about-cards">
-            <div className="about-card">
+            </Col>
+          </Container>
+        
+        
+          <Container className="col-lg-12" id="about-cards-outer" fluid>
+            <Col className="col-lg-4 about-cards">
               <Fade bottom cascade>
-                <div>
-                  <h1>Vision</h1>
-                </div>
-                <div>
-                  <p>We specialize in the integration of the latest assembly line automation and industrial automation technologies to deliver high-quality tooling solutions.</p>
+                <div className="about-card">
+                  <div>
+                    <h1>Mission</h1>
+                  </div>
+                  <div>
+                    <p>Our aim is to become the top industrial equipment supplier in India, offering a wide range of products that help reduce process costs.</p>
+                  </div>
                 </div>
               </Fade>
-            </div>
-          </Col>
-
-          <Col className="col-lg-4 about-cards">
-            <Fade bottom cascade>
+            </Col>
+            <Col className="col-lg-4 about-cards">
               <div className="about-card">
-                <div>
-                  <h1>Values</h1>
-                </div>
-                <div>
-                  <p>Prioritizing customer satisfaction, ensuring high-quality work, delivering on our commitments, and fostering strong collaborations with our business associates.</p>
-                </div>
+                <Fade bottom cascade>
+                  <div>
+                    <h1>Vision</h1>
+                  </div>
+                  <div>
+                    <p>We specialize in the integration of the latest assembly line automation and industrial automation technologies to deliver high-quality tooling solutions.</p>
+                  </div>
+                </Fade>
               </div>
-            </Fade>
-          </Col>
-        </Row>
+            </Col>
+
+            <Col className="col-lg-4 about-cards">
+              <Fade bottom cascade>
+                <div className="about-card">
+                  <div>
+                    <h1>Values</h1>
+                  </div>
+                  <div>
+                    <p>Prioritizing customer satisfaction, ensuring high-quality work, delivering on our commitments, and fostering strong collaborations with our business associates.</p>
+                  </div>
+                </div>
+              </Fade>
+            </Col>
+          </Container>
+        
       </Container>
 
       <Products />
--- src/components/navbar/NavMain.jsx
+++ src/components/navbar/NavMain.jsx
@@ -1,31 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import "./NavMain.css";
 import Logo from '/logo_main.png'
+import {AiOutlineMenuFold, AiOutlineMenuUnfold} from 'react-icons/ai';
 
-const NavMain = () => {
-  return (
-    <div className='nav-parent'>
-        <div className='nav-items'>
-            <div className='nav-logo'>
-                <img className='logo-img' src={Logo} alt="logo"/>
+const NavMain = (props) => {
+    const [sidebar, setSidebar] = useState(false);
+    const sidebarRef = useRef(null);
+
+    useEffect(() => {
+        
+        if (sidebar) sidebarRef.current.style.height = `${props.sidebarHeight}px`;
+
+    }, [sidebar]);
+
+    return (
+        <div className='nav-parent'>
+            <div className='nav-items'>
+                <div className='nav-logo'>
+                    <img className={`logo-img ${sidebar && 'hidden'}`} src={Logo} alt="logo"/>
+                </div>
+                <div className="sidebar-icon" onClick={() => setSidebar(!sidebar)}>
+                    <AiOutlineMenuFold />
+                </div>
+                <div className={`ul-elements ${sidebar && 'active'}`} ref={sidebarRef}>
+                    {
+                        sidebar && 
+
+                        <div className='sidebar-content-active'>
+                            <div className="sidebar-icon active" onClick={() => setSidebar(!sidebar)}>
+                                <AiOutlineMenuUnfold />
+                            </div>
+
+                            <div className='nav-logo'>
+                                <img className={`logo-img ${sidebar && 'active'}`} src={Logo} alt="logo"/>
+                            </div>
+                        </div>
+                    }
+                    <ul>
+                        <li>
+                            <a href="/"> Home </a>
+                        </li>
+                        <li>
+                            <a href="/#about-section"> About Us </a>
+                        </li>
+                        <li>
+                            <a href="/#products-page"> Our Products </a>
+                        </li>
+                        <li>
+                            <a href="#footer-main"> Contact Us </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <ul>
-                <li>
-                    <a href="/"> Home </a>
-                </li>
-                <li>
-                    <a href="/#about-section"> About Us </a>
-                </li>
-                <li>
-                    <a href="/#products-page"> Our Products </a>
-                </li>
-                <li>
-                    <a href="#footer-main"> Contact Us </a>
-                </li>
-            </ul>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NavMain
